bec7375707c128513e694f9729d243569cf45159
snelbops <mila.dig@yahoo.com>
2021-11-22 20:10:26 +0000

    Code for DB Connection
    Half of the code to connect with DB

--- src/crossover_ca/Crossover_CA.java
+++ src/crossover_ca/Crossover_CA.java
@@ -5,6 +5,11 @@
  */
 package crossover_ca;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author adminBeka
@@ -16,6 +21,26 @@ public class Crossover_CA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            Class.forName("com.mysql.cl.jdbc.Driver").newInstance();
+            String dbServer = "jdbc:mysql://localhost:336/Userstable";//Userstable will be the name of the SQLPtoject
+            String user = "root";
+            String password = "";//to set password if needed
+            String query = "Select * FROM user";// user is the name of the SQLtable
+            
+            // Get a connection to the database
+            Connection conn = DriverManager.getConnection(dbServer, user, password);
+            
+            // Get a statement from the connection
+            Statement stmt = conn.createStatement() ;
+            
+            // Execute the query
+            ResultSet rs = stmt.executeQuery(query) ;
+            
+            while(rs.next()){
+            }
+            
+        }catch (SQLExeption var8);
     }
     
 }
